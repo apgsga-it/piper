efcac2de6b061d012b1ffe0859cfcbefaacee012
Christoph Henrici <chhenrici@gmail.com>
2018-02-22 20:37:15 +0100

    Deleted versions-gradle-plugin Additional API for Versions File Bumped version to SNAPSHOT

---
+++ apg-patch-artifact-querymanager/src/main/java/com/apgsga/artifact/query/ArtifactVersionManager.java
@@ -0,0 +1,18 @@
+package com.apgsga.artifact.query;
+
+import java.net.URI;
+
+import com.apgsga.artifact.query.impl.PropertyFileBasedVersionManager;
+
+public interface ArtifactVersionManager {
+
+	public static ArtifactVersionManager create(String propertyVersionFilePath, URI mavenLocalPath,
+			String bomGroupId, String bomArtifactId, String bomVersionId) {
+		return new PropertyFileBasedVersionManager(propertyVersionFilePath, mavenLocalPath, bomGroupId, bomArtifactId,
+				bomVersionId);
+	}
+
+	public String getVersionFor(String group, String name);
+
+	public void updateVersion(String group, String name, String version);
+}
