6fc29eb7caeef92e226999e05e4d848dc4eb47c7
Christoph Henrici <christoph.henrici@chesnb.com>
2018-02-27 10:48:10 +0100

    Elimated temporary versions.properties file Elimated update API Bumped Version

--- apg-patch-artifact-querymanager/src/main/java/com/apgsga/artifact/query/ArtifactVersionManager.java
+++ apg-patch-artifact-querymanager/src/main/java/com/apgsga/artifact/query/ArtifactVersionManager.java
@@ -6,12 +6,11 @@ import com.apgsga.artifact.query.impl.PropertyFileBasedVersionManager;
 
 public interface ArtifactVersionManager {
 
-	public static ArtifactVersionManager create(String propertyVersionFilePath, URI mavenLocalPath,
-			String bomGroupId, String bomArtifactId, String bomVersionId) {
-		return new PropertyFileBasedVersionManager(propertyVersionFilePath, mavenLocalPath, bomGroupId, bomArtifactId);
+	public static ArtifactVersionManager create(String propertyVersionFilePath, URI mavenLocalPath, String bomGroupId,
+			String bomArtifactId, String bomVersionId) {
+		return new PropertyFileBasedVersionManager(mavenLocalPath, bomGroupId, bomArtifactId);
 	}
 
 	public String getVersionFor(String group, String name, String bomVersion);
 
-	public void updateVersion(String group, String name, String version);
 }
