7bad23601a6f8e597bf7bd9286d98b3711fae285
Christoph Henrici <christoph.henrici@chesnb.com>
2018-04-04 19:16:04 +0200

    Fix cvs permissions

--- apg-patch-service-server/packaging/rpm/pre-install.sh
+++ apg-patch-service-server/packaging/rpm/pre-install.sh
@@ -8,8 +8,8 @@ if [ "$1" = "2" ]; then
 	systemctl stop apg-patch-service-server
 fi
 if [ "$1" = "1" ]; then
-	echo "Creating group: apg-patch-service-server"
-	/usr/sbin/groupadd -f -r apg-patch-service-server 2> /dev/null || :
+	#echo "Creating group: apg-patch-service-server"
+	#/usr/sbin/groupadd -f -r apg-patch-service-server 2> /dev/null || :
 	echo "Creating user: apg-patch-service-server"
 	# TODO (che, 4.4.2018 ) : Temp fix for cvs right , resp AD domain user 
 	/usr/sbin/useradd -r -m -c "apg-patch-service-server user" apg-patch-service-server -g jenkins 2> /dev/null || :
