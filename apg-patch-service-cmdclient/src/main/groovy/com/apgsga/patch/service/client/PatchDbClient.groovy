936ebfc405197e04dbf890df5a34e9b68b5bb633
Christoph Henrici <christoph.henrici@chesnb.com>
2018-04-17 13:14:48 +0200

    Update PatchStatus

--- apg-patch-service-cmdclient/src/main/groovy/com/apgsga/patch/service/client/PatchDbClient.groovy
+++ apg-patch-service-cmdclient/src/main/groovy/com/apgsga/patch/service/client/PatchDbClient.groovy
@@ -2,13 +2,19 @@ package com.apgsga.patch.service.client
 import groovy.sql.Sql
 class PatchDbClient {
 	
+	def statusMap = [EntwicklungInstallationsbereit:2,InformatiktestInstallationsbereit:15, ProduktionInstallationsbereit:65, Entwicklung:0, Informatiktest:20, Produktion:80]
+	
 	public void executeStateTransitionAction(def dbProperties, def patchNumber, def toStatus) {
 		println "Not implemented yet for ${dbProperties} , ${patchNumber} and ${toStatus}"
-		def sql = Sql.newInstance(dbProperties.db.url, dbProperties.db.user, dbProperties.db.passwd)
-		def result = sql.execute("select * from cm_patch_f")
-		println "result: ${result}"
-		
+		def statusNum = statusMap[toStatus]
+		if (statusNum == null) {
+			println "Error , no Status mapped for ${toStatus}"
+			return
+		}
+		def dbConnection = Sql.newInstance(dbProperties.db.url, dbProperties.db.user, dbProperties.db.passwd)
+		def sql = "update cm_patch_f set status = ${statusNum} where id = ${patchNumber}"
+		println "Executing ${sql}"
+		def result = dbConnection.execute(sql)
+		println "Done with result: ${result}"
 	}
-		
-
 }
