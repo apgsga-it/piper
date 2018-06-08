63c2a9751c506d08cda04fba5db42213a76ce5f7
Julien Helbling <Julien Helbling@LAZHG552433012>
2018-06-08 14:26:01 +0200

    Using a post-install script to create /var/opt/apg-patch-cli folder.

---
+++ apg-patch-service-cmdclient/packaging/rpm/post-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# post-install.sh
+#
+echo "Post Install script"
+
+
+if [ ! -d "/var/opt/apg-patch-cli" ]; then
+
+    echo "Creating and setting permissions on /var/opt/apg-patch-cli"
+	mkdir /var/opt/apg-patch-cli
+	chown -R jenkins:jenkins /var/opt/apg-patch-cli 
+
+else
+
+	echo "/var/opt/apg-patch-cli already exist."
+
+fi
+
+exit 0
