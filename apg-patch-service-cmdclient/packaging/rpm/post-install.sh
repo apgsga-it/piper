6b843beacb080c855965632e534ac87e5934c10a
Julien Helbling <Julien Helbling@LAZHG552433012>
2018-06-11 10:14:54 +0200

    chown done also when directory already exists.

--- apg-patch-service-cmdclient/packaging/rpm/post-install.sh
+++ apg-patch-service-cmdclient/packaging/rpm/post-install.sh
@@ -8,12 +8,13 @@ if [ ! -d "/var/opt/apg-patch-cli" ]; then
 
     echo "Creating and setting permissions on /var/opt/apg-patch-cli"
 	mkdir /var/opt/apg-patch-cli
-	chown -R jenkins:jenkins /var/opt/apg-patch-cli 
 
 else
 
 	echo "/var/opt/apg-patch-cli already exist."
 
 fi
+
+chown -R jenkins:jenkins /var/opt/apg-patch-cli
 
 exit 0
