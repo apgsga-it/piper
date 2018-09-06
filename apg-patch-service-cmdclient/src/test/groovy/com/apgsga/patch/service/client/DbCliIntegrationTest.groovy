e6cdafb3ef8d5b6e18c3fc9932211817aadbcd52
Julien Helbling <Julien Helbling@LAZHG552433012>
2018-09-06 17:21:45 +0200

    ignore back

--- apg-patch-service-cmdclient/src/test/groovy/com/apgsga/patch/service/client/DbCliIntegrationTest.groovy
+++ apg-patch-service-cmdclient/src/test/groovy/com/apgsga/patch/service/client/DbCliIntegrationTest.groovy
@@ -29,6 +29,7 @@ class DbCliIntegrationTest extends Specification {
 		result.results.size() == 0
 	}
 	
+	@Ignore("TODO address db preconditions")
 	def "Patch DB Cli returns patch ids to be re-installed after a clone"() {
 		setup:
 			def patchDbCli = PatchDbCli.create()
