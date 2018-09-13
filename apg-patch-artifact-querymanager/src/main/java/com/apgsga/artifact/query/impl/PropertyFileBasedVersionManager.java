4c35f705e2207c7dcdae94ccf364ab745b147e53
Christoph Henrici <christoph.henrici@chesnb.com>
2018-09-13 22:19:23 +0200

    PropertyFileBasedVersionManager Property Merge

--- apg-patch-artifact-querymanager/src/main/java/com/apgsga/artifact/query/impl/PropertyFileBasedVersionManager.java
+++ apg-patch-artifact-querymanager/src/main/java/com/apgsga/artifact/query/impl/PropertyFileBasedVersionManager.java
@@ -2,25 +2,36 @@ package com.apgsga.artifact.query.impl;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import org.assertj.core.util.Lists;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.eclipse.aether.resolution.ArtifactResolutionException;
 import org.eclipse.aether.resolution.DependencyResolutionException;
 
 import com.apgsga.artifact.query.ArtifactManager;
 import com.apgsga.artifact.query.ArtifactVersionManager;
+import com.apgsga.microservice.patch.api.MavenArtifact;
 import com.apgsga.microservice.patch.exceptions.ExceptionFactory;
 
 public class PropertyFileBasedVersionManager implements ArtifactVersionManager {
 
 	private ArtifactManager artifactManager;
 	private Properties versionsProperties;
+	private List<Map<String,String>> mavenArtefactsToOverride = Lists.emptyList();
 
 	public PropertyFileBasedVersionManager(URI mavenLocalPath, String bomGroupId, String bomArtifactId) {
 		super();
 		this.artifactManager = ArtifactManager.create(bomGroupId, bomArtifactId, mavenLocalPath.getPath());
 
+	}
+	
+	public PropertyFileBasedVersionManager(URI mavenLocalPath, String bomGroupId, String bomArtifactId, List<Map<String,String>>  mavenArtefactsToOverride) {
+		super();
+		this.artifactManager = ArtifactManager.create(bomGroupId, bomArtifactId, mavenLocalPath.getPath());
+		this.mavenArtefactsToOverride = mavenArtefactsToOverride;
 	}
 
 	@Override
@@ -32,9 +43,24 @@ public class PropertyFileBasedVersionManager implements ArtifactVersionManager {
 
 	private synchronized Properties getProperties(String bomVersion) {
 		if (versionsProperties == null) {
+			Properties overrideVersionProperties = convertToProperties(mavenArtefactsToOverride); 
 			versionsProperties = intialLoad(artifactManager,bomVersion);
+			for (Object key : overrideVersionProperties.keySet()) {
+				versionsProperties.put(key, overrideVersionProperties.get(key)); 
+			}
 		}
 		return versionsProperties;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Properties convertToProperties(List<Map<String,String>> mavenArtefactsToOverride) {
+		final Properties properties = new Properties();
+		for (Map<String,String> art : mavenArtefactsToOverride) {
+			if (!art.get("version").endsWith("SNAPSHOT")) {
+				properties.put(art.get("groupId") + ":" + art.get("artifactId"), art.get("version")); 
+			}
+		}
+		return properties; 
 	}
 
 	private static Properties intialLoad(ArtifactManager artifactManager,String bomVersion) {
