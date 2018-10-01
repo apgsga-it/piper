2b5694c4faefdb4bacbe4be917f07c49c1efe332
Julien Helbling <Julien Helbling@LAZHG552433012>
2018-10-01 15:19:34 +0200

    comment removed

--- apg-patch-service-cmdclient/src/main/groovy/com/apgsga/patch/service/client/PatchArtifactoryClient.groovy
+++ apg-patch-service-cmdclient/src/main/groovy/com/apgsga/patch/service/client/PatchArtifactoryClient.groovy
@@ -17,8 +17,6 @@ class PatchArtifactoryClient {
 	
 	private final String DB_PATCH_REPO = "dbpatch"
 	
-	private final String FIRST_PART_FOR_ARTIFACT_SEARCH = "T-"
-	
 	def PatchArtifactoryClient(def configuration) {
 		config = configuration
 		artifactory = ArtifactoryClientBuilder.create().setUrl(config.artifactory.url).setUsername(config.artifactory.user).setPassword(config.artifactory.password).build();
@@ -37,18 +35,6 @@ class PatchArtifactoryClient {
 		};
 	}
 	
-	// TODO JHE (26.06.2018): will be remove with JAVA8MIG-389 
-	// TODO JHE: To be deleted -> we don't have "T" releases anymore !!!
-	/*
-	public def deleteAllTRevisions(def dryRun) {
-		println "Removing all T Artifact from Artifactory."
-		removeArtifacts("*-T-*", dryRun);
-		if(!dryRun) {
-			new File(config.revision.file.path).delete()
-		}
-	}
-	*/
-	
 	def cleanReleases(def target) {
 		
 		def revisionClient = new PatchRevisionClient(config)
@@ -63,39 +49,18 @@ class PatchArtifactoryClient {
 				// Will delete all published sources jar for the given version/revision
 				removeArtifacts("*-${it}-sources.jar", dryRun)
 			}
-		}
-		
-		/*
-		def PatchRevisionClient patchRevisionClient = new PatchRevisionClient(config)
-		def revisionsToBeDeleted = patchRevisionClient.getInstalledRevisions(target)
-		// JHE (26.07.2018): If lastRevision is null, it means that nothing has ever been patch on the target -> then we don't have to do anything.
-		// JHE (13.08.2018): If lastRevision ends with "@P", it means nothing has been patched since last clone -> then we don't have to do anything.
-		if(lastRevision != 'SNAPSHOT') {
-		 
-			def rangeStep = config.revision.range.step
-			def from = ((int) (Long.valueOf(lastRevision) / rangeStep)) * rangeStep
-			def dryRun = config.onclone.delete.artifact.dryrun
-			def originalFrom = from
-			
-			println("Artifact from ${from} to ${lastRevision} will be deleted from Artifactory.")
-			
-			// TODO JHE: We can probably improve (remove) this loop by using a more sophisticated Regex
-			while(from <= Long.valueOf(lastRevision)) {
-				// TODO JHE: do we want to search in more repos? Is "realeases" enough?
-				removeArtifacts("*${FIRST_PART_FOR_ARTIFACT_SEARCH}${from}*", dryRun)
-				from++
-			}
-			
+					
 			// Cleaning Docker Image as well
+			// TODO JHE: Uncomment and adapt it as soon as JAVA8MIG-375 will be solved.
+			/*
 			if(!dryRun) {
 				def jadasCleanupCmd = "/opt/apgops/cleanup_jadas_images.sh ${originalFrom} ${lastRevision}"
 				println "Following command will be started to clean Jadas images : ${jadasCleanupCmd}"
 				['bash', '-c', jadasCleanupCmd].execute().getOutputStream().println()
 				println "Jadas Images have been deleted for ${target}"
 			}
-			
-		}
-		*/
+			*/
+		}			
 		else {
 			println("No release to clean for ${target}. We probably never have any patch installed directly on ${target}, or no patch has been newly installed since last clone.")
 		}
