b8accecdcef5c587f527f18ce245853fc7245b30
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-06-25 14:36:26 +0200

    packaging adapted

--- apg-patch-service-cmdclient/pkg/zip/apscli.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -Dspring.profiles.active=live,remotecvs,groovyactions -cp "lib/apg-patch-cli-fat.jar:." -Dlogback.statusListenerClass=ch.qos.logback.core.status.NopStatusListener -DappPropertiesFile=conf/application.properties pliStarter "$@"
--- apg-patch-service-cmdclient/src/main/groovy/pliStarter.groovy
+++ apg-patch-service-cmdclient/src/main/groovy/pliStarter.groovy
@@ -1,20 +1,3 @@
 import com.apgsga.patch.service.client.PatchCli
-import com.apgsga.patch.service.client.db.PatchDbCli
 
-def client = args[0]
-
-// We remove with "pli", "pliDb" or "pliRev from args as they have no meaning for PatchCli or PatchDbCli
-args = args - "pli"
-args = args - "pliDb"
-
-
-if(client.equalsIgnoreCase("pli")) {
-	System.exit(PatchCli.create(client).process(args).returnCode)
-}
-
-if(client.equalsIgnoreCase("pliDb")) {
-	System.exit(PatchDbCli.create().process(args).returnCode)
-}
-
-println "pliStarter couldn't find an pli to be started with name ${client}."
-System.exit(1)
+System.exit(PatchCli.create().process(args).returnCode)
