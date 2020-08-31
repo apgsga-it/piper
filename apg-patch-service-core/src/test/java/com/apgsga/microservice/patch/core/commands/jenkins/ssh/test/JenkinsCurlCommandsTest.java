d5998f3fdf7414e49148896466338c8c42e4ccc3
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-08-31 12:26:13 +0200

    test case for curl command

--- apg-patch-service-core/src/test/java/com/apgsga/microservice/patch/core/commands/jenkins/ssh/test/JenkinsCurlCommandsTest.java
+++ apg-patch-service-core/src/test/java/com/apgsga/microservice/patch/core/commands/jenkins/ssh/test/JenkinsCurlCommandsTest.java
@@ -3,23 +3,25 @@ package com.apgsga.microservice.patch.core.commands.jenkins.ssh.test;
 import com.apgsga.microservice.patch.core.commands.CommandRunner;
 import com.apgsga.microservice.patch.core.commands.ProcessBuilderCmdRunnerFactory;
 import com.apgsga.microservice.patch.core.commands.jenkins.curl.JenkinsCurlCommand;
-import com.apgsga.microservice.patch.core.commands.jenkins.ssh.JenkinsSshCommand;
-import org.junit.Assert;
+import groovy.json.JsonSlurper;
 import org.junit.Test;
+import org.springframework.util.Assert;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
+import java.util.Map;
 
 public class JenkinsCurlCommandsTest extends JenkinsCliBaseTest {
 
     @Test
-    public void test() {
-
+    public void testJenkinsCurlGetLastBuildNumber() {
         JenkinsCurlCommand jenkinsCurlCmd = JenkinsCurlCommand.createJenkinsCurlGetLastBuildCmd(JENKINS_URL,JENKINS_SSH_USER,JENKINS_SSH_USER,JOB_NAME_WITHOUT_FILE_PARAM);
         ProcessBuilderCmdRunnerFactory runnerFactory = new ProcessBuilderCmdRunnerFactory();
         CommandRunner runner = runnerFactory.create();
         List<String> result = runner.run(jenkinsCurlCmd);
-        System.out.println(result);
-
-        //Assert.assertTrue("Returned message should contain SUCCESS",result.stream().anyMatch(c -> {return c.contains("Started " + JOB_NAME_WITHOUT_FILE_PARAM + " #");}));
+        JsonSlurper js = new JsonSlurper();
+        Map parsedData = (Map) js.parse(new ByteArrayInputStream(result.get(0).getBytes()));
+        Map lastBuild = (Map) parsedData.get("lastBuild");
+        Assert.isTrue(Integer.valueOf(String.valueOf(lastBuild.get("number"))) > 0,"Last Build number couldn not be corrrectly retrieved");
     }
 }
