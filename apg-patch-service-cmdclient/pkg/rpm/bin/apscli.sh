a0e33b33a7e3d375f8feb15c67892615b1d370ca
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-09-29 06:08:08 +0200

    apscli: no more logback config windows new line removed

--- apg-patch-service-cmdclient/pkg/rpm/bin/apscli.sh
+++ apg-patch-service-cmdclient/pkg/rpm/bin/apscli.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Dlogback.statusListenerClass=ch.qos.logback.core.status.NopStatusListener -jar /opt/apg-patch-cli/bin/apg-patch-cli-fat.jar $@
+java -jar /opt/apg-patch-cli/bin/apg-patch-cli-fat.jar $@
