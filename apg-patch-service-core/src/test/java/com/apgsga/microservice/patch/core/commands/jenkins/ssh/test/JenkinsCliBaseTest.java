64814a6605945407946f71cd2bd0fe8d1f3653de
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-10-01 11:18:56 +0200

    investigate why fileParameter is empty

--- apg-patch-service-core/src/test/java/com/apgsga/microservice/patch/core/commands/jenkins/ssh/test/JenkinsCliBaseTest.java
+++ apg-patch-service-core/src/test/java/com/apgsga/microservice/patch/core/commands/jenkins/ssh/test/JenkinsCliBaseTest.java
@@ -8,7 +8,7 @@ public abstract class JenkinsCliBaseTest {
 
     public static final String JENKINS_SSH_USER = "jhe";
     public static final String JENKINS_USER_TOKEN = "1151e35f8912cbcb5754d7320b54e4264c";
-    public static final String JENKINS_HOST = "172.18.43.76";
+    public static final String JENKINS_HOST = "172.17.206.117";
     public static final String JENKINS_PORT = "8080";
     public static final String JENKINS_SSH_PORT = "53801";
     public static final String JENKINS_URL = "http://" + JENKINS_HOST + ":" + JENKINS_PORT;
