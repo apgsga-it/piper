ebacde2bda1b77628add5e23e9bc58c8fdd6a05e
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-10-13 10:03:01 +0200

    initializing PatchAction Map

--- apg-patch-service-core/src/main/java/com/apgsga/microservice/patch/core/impl/PatchActionExecutor.java
+++ apg-patch-service-core/src/main/java/com/apgsga/microservice/patch/core/impl/PatchActionExecutor.java
@@ -1,9 +1,7 @@
 package com.apgsga.microservice.patch.core.impl;
 
-import java.util.Map;
-
 public interface PatchActionExecutor {
 	
-	public void execute(String patchNumber, String toStatus, Map<String,PatchAction> action);
+	public void execute(String patchNumber, String toStatus);
 
 }
--- apg-patch-service-core/src/main/java/com/apgsga/microservice/patch/core/impl/PatchActionExecutorImpl.java
+++ apg-patch-service-core/src/main/java/com/apgsga/microservice/patch/core/impl/PatchActionExecutorImpl.java
@@ -3,10 +3,10 @@ package com.apgsga.microservice.patch.core.impl;
 import com.apgsga.microservice.patch.api.Stage;
 import com.apgsga.microservice.patch.api.StageMapping;
 import com.apgsga.microservice.patch.exceptions.Asserts;
+import com.apgsga.microservice.patch.exceptions.ExceptionFactory;
 import com.google.common.collect.Maps;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.MutablePropertyValues;
 
 import java.util.Map;
 
@@ -14,7 +14,13 @@ public class PatchActionExecutorImpl implements PatchActionExecutor {
 
 	protected static final Log LOGGER = LogFactory.getLog(PatchActionExecutorImpl.class.getName());
 
+	public static final String ENTWICKLUNG_INSTALLATIONSBEREIT_ACTION = "EntwicklungInstallationsbereitAction";
+
+	public static final String PIPELINE_INPUT_ACTION = "PipelineInputAction";
+
 	private SimplePatchContainerBean patchContainer;
+
+	private Map<String,PatchAction> patchActions;
 
 	public PatchActionExecutorImpl() {
 		super();
@@ -23,10 +29,31 @@ public class PatchActionExecutorImpl implements PatchActionExecutor {
 	public PatchActionExecutorImpl(SimplePatchContainerBean patchContainer) {
 		super();
 		this.patchContainer = patchContainer;
+		initPatchActions();
+	}
+
+	private void initPatchActions() {
+		patchActions = Maps.newHashMap();
+		patchContainer.getMetaInfoRepo().stageMappings().getStageMappings().forEach(stageMapping -> {
+			stageMapping.getStages().forEach(stage -> {
+				patchActions.put(stageMapping.getName() + stage.getToState(), getAction(stage.getImplcls()));
+			});
+		});
+	}
+
+	private PatchAction getAction(String implcls) {
+		//TODO JHE (13.10.2020): One could do better that if/else ... but ok for now
+		if(implcls.contains(ENTWICKLUNG_INSTALLATIONSBEREIT_ACTION)) {
+			return new EntwicklungInstallationsbereitAction(patchContainer);
+		}
+		if(implcls.contains(PIPELINE_INPUT_ACTION)) {
+			return new PipelineInputAction(patchContainer);
+		}
+		throw ExceptionFactory.createPatchServiceRuntimeException("PatchActionExecutorImpl.implcls.exception",new Object[]{implcls});
 	}
 
 	@Override
-	public void execute(String patchNumber, String toStatus, Map<String,PatchAction> patchActions) {
+	public void execute(String patchNumber, String toStatus) {
 		Asserts.notNullOrEmpty(patchNumber, "PatchActionExecutorImpl.execute.patchnumber.notnullorempty.assert", new Object[] {toStatus });
 		Asserts.isTrue((patchContainer.getRepo().patchExists(patchNumber)),"PatchActionExecutorImpl.execute.patch.exists.assert", new Object[] { patchNumber, toStatus });
 		StageMapping stageMapping = patchContainer.getMetaInfoRepo().stageMappingFor(toStatus);
@@ -40,4 +67,5 @@ public class PatchActionExecutorImpl implements PatchActionExecutor {
 		PatchAction pa = patchActions.get(toStatus);
 		pa.executeToStateAction(patchNumber,toStatus,parameters);
 	}
+
 }
