19347f2570b0ba776b8d3d44c44feacfef80ab4c
Christoph Henrici <chhenrici@gmail.com>
2020-11-19 15:46:41 +0100

    Rpm pre install , check if user is already created, see puppet server setup

--- apg-patch-service-server/packaging/rpm/pre-install.sh
+++ apg-patch-service-server/packaging/rpm/pre-install.sh
@@ -8,12 +8,16 @@ if [ "$1" = "2" ]; then
 	systemctl stop apg-patch-service-server
 fi
 if [ "$1" = "1" ]; then
-	echo "Creating group: apg-patch-service-server"
-	/usr/sbin/groupadd -f -r apg-patch-service-server 2> /dev/null || :
-	echo "Creating user: apg-patch-service-server"
-	/usr/sbin/useradd -r -m -c "apg-patch-service-server user" apg-patch-service-server -g apg-patch-service-server 2> /dev/null || :
-	echo "apg-patch-service-server:apg-patch-service-server" | sudo chpasswd
-	echo "Password has been set for apg-patch-service-server user, you might want to change it."
+  if id "apg-patch-service-server" &>/dev/null; then
+    echo 'apg-patch-service-server already exists '
+  else
+	  echo "Creating group: apg-patch-service-server"
+	  /usr/sbin/groupadd -f -r apg-patch-service-server 2> /dev/null || :
+	  echo "Creating user: apg-patch-service-server"
+	  /usr/sbin/useradd -r -m -c "apg-patch-service-server user" apg-patch-service-server -g apg-patch-service-server 2> /dev/null || :
+	  echo "apg-patch-service-server:apg-patch-service-server" | sudo chpasswd
+	  echo "Password has been set for apg-patch-service-server user, you might want to change it."
+	fi
 fi
 
 sudoFile="/etc/sudoers.d/apg-patch-service-server"
