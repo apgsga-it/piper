2f3bd0eee20e20c4acbf08523699f6695adada0b
Christoph Henrici <chhenrici@gmail.com>
2021-03-18 12:04:28 +0100

    cvs scripts cvs_root parameter

--- apg-patch-service-server/packaging/bin/branchWorkaround.sh
+++ apg-patch-service-server/packaging/bin/branchWorkaround.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+  echo "Wrong number of parameter, CVS_ROOT as parameter expected '"
+  exit
+fi
+
+export CVS_ROOT=$1
+
 for myDiff in {1..3}; do
   myTag=Ms_patch_$( expr ${myDiff} + $( ls -al /var/opt/apg-patch-service-server/db/Patch*.json | grep -iv log | cut -d '/' -f6 | cut -d 'h' -f2 | cut -d '.' -f1 | tail -n1 ))
 
@@ -11,7 +18,7 @@ for myDiff in {1..3}; do
     touch /tmp/doneBranchWorkaround${myTag}
     for myDbModule in $( cat /var/opt/apg-patch-service-server/metaInfoDb/DbModules.json | grep -v '{' | grep -v '}' | grep -v 'dbModules' | grep -v ']' | tr -d '",\t\r' | tr '\n' ' ' ); do
       echo "`date +%Y/%m/%d-%H:%M:%S` - Branching to \"${myTag}\" on Module \"${myDbModule}\"";
-      ssh <%= $cvs_user %>@<%= piper_cvs_remote_host %> cvs -d /var/local/cvs/root rtag -r prod -b ${myTag} ${myDbModule}
+      ssh cvs rtag -r prod -b ${myTag} ${myDbModule}
     done
   fi
 done
