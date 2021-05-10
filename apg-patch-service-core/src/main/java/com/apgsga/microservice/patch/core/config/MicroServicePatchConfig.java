35aa62867fcc37bc4b1d2fada58e20ff859b5c1d
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-05-10 17:02:57 +0200

    patchConflictsChecker should not be a singleton

--- apg-patch-service-core/src/main/java/com/apgsga/microservice/patch/core/config/MicroServicePatchConfig.java
+++ apg-patch-service-core/src/main/java/com/apgsga/microservice/patch/core/config/MicroServicePatchConfig.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -169,6 +170,7 @@ public class MicroServicePatchConfig {
 	}
 
 	@Bean(name = "patchConflictsChecker")
+	@Scope("prototype")
 	public PatchConflictsChecker patchConflictsChecker() {
 		return PatchConflictsCheckerImpl.create();
 	}
